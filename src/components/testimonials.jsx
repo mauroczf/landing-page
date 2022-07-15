8dad06dde05febdf14ad685ccfafc3088d040146
mauroczf <mauroczf@gmail.com>
2022-07-15 10:29:47 -0400

    Fix

---
+++ src/components/testimonials.jsx
@@ -0,0 +1,11 @@
+export const Testimonials = (props) => {
+  return (
+    <div id='testimonials'>
+      <div className='container'>
+        <div className='section-title text-center'>
+          <h2>Lorem ipsun dolor sit amet</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
